13a69cd2563d07696da6e742f7fca64ee4aea246
“amarasagekar” <amarasagekar@gmail.com>
2024-07-15 22:23:56 +0530

    initial setup

---
+++ SwiftUI_NewsApp/SwiftUI_NewsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_NewsApp
+//
+//  Created by AMAR on 15/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_NewsApp/SwiftUI_NewsApp/SwiftUI_NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NewsAppApp.swift
+//  SwiftUI_NewsApp
+//
+//  Created by AMAR on 15/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
